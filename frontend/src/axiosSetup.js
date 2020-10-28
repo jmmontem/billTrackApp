c71382193c0d26a099181b379ae8070778d05b5e
Juan Miguel Montemayor <jumontemayor.j@gmail.com>
2020-10-28 01:27:46 -0700

    Fixing

--- frontend/src/axiosSetup.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const axiosS = axios.create({
-  baseURL: 'https://bill-track-api.herokuapp.com/',
-  timeout: 1000,
-  headers: {'X-Custom-Header': 'foobar'}
-});
