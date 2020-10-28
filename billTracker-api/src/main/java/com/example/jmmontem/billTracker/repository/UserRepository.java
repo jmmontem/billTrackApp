29e4bdd22b076944096e119f81d0a1983cbe9ba4
Juan Miguel Montemayor <jumontemayor.j@gmail.com>
2020-10-28 01:25:44 -0700

    Fixing Folders

--- billTracker-api/src/main/java/com/example/jmmontem/billTracker/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jmmontem.billTracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.jmmontem.billTracker.model.User;
-
-public interface UserRepository extends JpaRepository<User,	String>{
-	
-}
